34ea03e0fa12604fbe3339d82ca2497ea1b9f5e3
Avinash219 <avinash.dev21987@gmail.com>
2020-04-10 21:27:59 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const randomString = (len = 16) => {
+    try {
+        if (isNaN(len)) throw 'Only numbers are allowed';
+        let str = '';
+
+        let startIndex = 97
+        let endIndex = 122
+
+        while (len > 0) {
+            let char = (Math.floor(Math.random() * (endIndex - startIndex + 1)) + startIndex)
+            str += String.fromCharCode(char)
+            len--;
+        }
+        return str;
+
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+console.log(randomString('abc'))
